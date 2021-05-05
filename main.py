1f12eef2c8994692aeb9e70b56dd6efc7430cd0b
pavankumarallu <vamsipavankumarallu@gmail.com>
2021-05-05 09:24:51 +0530

    FirstCommit

---
+++ main.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkinter import ttk
+from PIL import Image,ImageTk
+ 
+
+class FaceRecognitionSystem:
+    def __init__(self,root):
+        self.root = root
+        self.root.geometry("1530x790+0+0")
+        self.root.title("Face Recognition System")
+       
+        img2 = Image.open(r"E:\FaceRecogniseSystem\images\bg.jpg")
+        img2 = img2.resize((1530,790),Image.ANTIALIAS)
+        self.photoimage1 = ImageTk.PhotoImage(img2)
+        bg = Label(self.root,image = self.photoimage1)
+        bg.place(x = 0,y = 0,width = 1530,height = 790)
+
+
+
+if __name__ == "__main__":
+    root = Tk()
+    frs = FaceRecognitionSystem(root)
+    root.mainloop()
